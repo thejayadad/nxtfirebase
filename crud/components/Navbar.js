957c44ec3f2a075554821e4cc43c541d2133309f
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-05-20 12:42:56 -0400

    update

--- crud/components/Navbar.js
+++ crud/components/Navbar.js
@@ -2,7 +2,39 @@
 import Link from 'next/link';
 
 export default function Navbar() {
+    const user = null;
+    const username = null;
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <button>Feed</button>
+                    </Link>
+                </li>
+                {username && (
+                    <>
+                    <li>
+                    <Link href="/admin">
+                        <button>Write Post</button>
+                    </Link>
+                    </li>
+                    <li>
+                    <Link href={`/${username}`}>
+                        <img src={user?.photoURL} />
+                    </Link>
+                    </li>
+                    </>
+                )}
 
-
-
+                {!username && (
+                        <li>
+                        <Link href="/enter">
+                            <button>Login</button>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
 }
